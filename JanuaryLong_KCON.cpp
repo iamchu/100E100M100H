be4b88d199627a127ca41f82e1919e3529259320
Vic <v235711@gmail.com>
2018-01-12 10:22:58 -0200

    %

--- JanuaryLong_KCON.cpp
+++ JanuaryLong_KCON.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxSubArraySum(int a[], int size)
+int maxSubArraySum(long long int some_array[], long long int size)
 {
-   int max_so_far = a[0];
-   int curr_max = a[0];
+   long long int max_so_far = some_array[0];
+   long long int curr_max = some_array[0];
  
    for (int i = 1; i < size; i++)
    {
-        curr_max = max(a[i], curr_max+a[i]);
+        curr_max = max(some_array[i], curr_max+some_array[i]);
         max_so_far = max(max_so_far, curr_max);
    }
    return max_so_far;
 }
 
 int main(){
-	int t,n,k;
+	long long int t,n,k;
 	cin>>t;
 	while(t--)
 	{
 		cin>>n>>k;
-		int a[n];
-		int b[n*k];
+		long long int a[n];
+		long long int b[n*k];
 		for(int i=0;i<n;i++)
 		{
 			cin>>a[i];
@@ -32,11 +32,12 @@ int main(){
 			b[j] = a[j%n];
 			// cout<<b[j]<<", ";
 		}
-		
-		int my_sum;
+
+		long long int my_sum;
 		if(n>1 || k>1)
 		{
-			my_sum = maxSubArraySum(b, n*k);
+			int b_size = sizeof(b)/sizeof(b[0]);
+			my_sum = maxSubArraySum(b, b_size);
 		}
 		else
 		{
